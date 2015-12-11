abb65f439a8e62930b87be020451db96363b3ede
Jimmy <jf.startupproject@gmail.com>
2015-12-11 23:31:15 +0000

    edit index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.sendFile('./index.html');
-//  var buf = fs.readFileSync('./index.html')
+//  response.sendFile('./index.html');
+  var buf = fs.readFileSync('./index.html','utf8');
+  response.send( buf );
 })
 
 app.listen(app.get('port'), function() {
