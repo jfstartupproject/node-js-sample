eedc7d9dec566ab9a10d4a9a0d301e06dfca6b1f
Jimmy <jf.startupproject@gmail.com>
2015-12-11 23:49:43 +0000

    edit lagi index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
+var buf = new Buffer(1000)
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('./index.html');
+  buf = fs.readFileSync('./index.html');
   response.end(buf.toString());
 })
 
