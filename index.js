7c38b1dd2eb0402a739cd52505cd9c2a87647389
Jimmy <jf.startupproject@gmail.com>
2015-12-12 00:15:08 +0000

    edit lagi index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var express = require('express')
 var app = express()
-var fs = require("fs");
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-//app.get('/', function(request, response) {
+app.get('/', function(request, response) {
   var buf = fs.readFileSync('index.html');
-//  response.end(buf.toString());
-  console.log (buf.toString());
-//})
+  response.send(buf.toString());
+//  console.log (buf.toString());
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
